a6a3148e52f4d8ddde81d3a957e18d3224bc20c3
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-06-25 09:55:25 +0100

    add services content template

--- public/index.php
+++ public/index.php
@@ -3,6 +3,38 @@ $title = "Home - QualityBuilders";
 require "../inc/head.php";
 ?>
 
-  
+<div class="container">
+    <div class="services-title">
+        <h2>Our Services</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Somnis iste natus. voluptatem accusantium</p>
+    </div>
+    <div class="services-cards">
+        <a href="#">
+            <img src alt>
+            <div class="service-description">
+                <h3>Architecture</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Somnis iste natus.</p>
+            </div>
+            <div class="service-icon arc"></div>
+        </a>
+        <a href="#">
+            <img src alt>
+            <div class="service-description">
+                <h3>Construction</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Somnis iste natus.</p>
+            </div>
+            <div class="service-icon con"></div>
+        </a>
+        <a href="#">
+            <img src alt>
+            <div class="service-description">
+                <h3>Architecture</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Somnis iste natus.</p>
+            </div>
+            <div class="service-icon ins"></div>
+        </a>
+    </div>
+</div>
+
 </body>
 </html>
